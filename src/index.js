9e946ff9492395100bca13e622dcd248ba725974
AnastaciusX <anastacius@users.noreply.github.com>
2025-12-12 00:04:34 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import  createItem  from './services/item.js';
-
+import * as cartService from './services/cart.js';
 const carrinho = [];
+const wishList = [];
 
-const item1 = await createItem('Banana', 2.5, 4);
+const item1 = await createItem('Banana', 2.5, 8);
 carrinho.push(item1);
 
-const item2 = await createItem('Maçã', 3.0, 2);
+const item2 = await createItem('Maçã', 3.0, 20);
 carrinho.push(item2);
 
-console.log('Carrinho de Compras:');
-carrinho.forEach((item, index) => {
-    console.log(`${index + 1}. ${item.name} - Preço: R$${item.price.toFixed(2)} - Quantidade: ${item.quantity} - Subtotal: R$${item.subtotal().toFixed(2)}`);
-});
+await printCart(carrinho);
 
-console.log('Total do Carrinho: R$' + carrinho.reduce((total, item) => total + item.subtotal(), 0).toFixed(2));
+await cartService.addItems(wishList, await createItem('Laranja', 4.0, 30));
+await cartService.addItems(carrinho, await createItem('esponja', 3.99, 50));
+
+await printCart(carrinho);
+
+cartService.deleteItem(carrinho, 'Maçã');
+
+await printCart(carrinho);
+
+
+async function printCart(cart) {
+    console.log('\nCarrinho de Compras:');
+    cart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - Preço: R$${item.price.toFixed(2)} - Quantidade: ${item.quantity} - Subtotal: R$${item.subtotal().toFixed(2)}`);
+    });
+    cartService.calculateTotal(cart).then(total => {
+        console.log('Total do Carrinho: R$' + total.toFixed(2));
+    });
+}
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,17 +1,26 @@
-async function addItems(userCart) {
-    
-}
-
-async function removeItems(userCart, index) {
-    
-}
-
-async function deleteItem(userCart, name) {
-    
+async function addItems(userCart, item) {
+    userCart.push(item);
 }
 
 async function calculateTotal(userCart) {
-    
+    return userCart.reduce((total, item) => {
+        return total + item.subtotal();
+    }, 0);
+}
+
+
+
+async function removeItems(userCart, index) {
+
+}
+
+async function deleteItem(userCart, name) {
+   const index = userCart.findIndex(item => item.name === name);
+   if (index !== -1) {
+       userCart.splice(index, 1);
+   }
+   return userCart;
+
 }
 
 export { addItems, removeItems, deleteItem, calculateTotal };
